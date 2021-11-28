316b61f53a37856cc4dbbf2e276896bac725ab06
djamelkorei <korei.djamel.eddine@gmail.com>
2021-11-28 20:40:17 +0100

    initial commit

---
+++ src/main/java/me/djamelkorei/springbootjpadatatable/SpringBootJpaDatatableApplication.java
@@ -0,0 +1,13 @@
+package me.djamelkorei.springbootjpadatatable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaDatatableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaDatatableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/djamelkorei/springbootjpadatatable/SpringBootJpaDatatableApplicationTests.java
@@ -0,0 +1,13 @@
+package me.djamelkorei.springbootjpadatatable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaDatatableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
